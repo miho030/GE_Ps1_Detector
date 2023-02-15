04c23f16bcb085c2fdc2e70644a7113a2e67599d
AOI <miho0_0@naver.com>
2023-02-15 16:01:33 +0800

    * fix bugs

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 # import libs <python libs>
 from clint.textui import puts, colored, indent
 from pyuac import main_requires_admin
+
 from ge_Const.ge_PrintUI import _printUi
 from ge_getXML._ge_load_Api import _runCleaner
 from ge_getXML._ge_load_Api import _runCollector
@@ -15,15 +16,26 @@ def _initToAdmin():
     __runAlgorithmLoop()
 
 def __runAlgorithmLoop():
-    with indent(4, quote=">>>"):
-        bRet, strCleanLogBuf = _runCleaner()
-        bRes, strColecLogBuf = _runCollector()
-        if (bRet != True) & (bRes != True):
-            puts(colored.red('[ERROR] Failure load external file') + str("FAILED to Execute Clearner.bat, Collector.bat"))
-        else:
-            puts(colored.green('Load external file') + str("Successfully and run Cleaner.bat, Collector.bat"))
-            strMalDir = "../malicious"
-            _ScanPs1_Mug(strMalDir)
+    bRet, strCleanLogBuf = _runCleaner()
+    bRes, strColecLogBuf = _runCollector()
+
+    if (bRet != True) & (bRes != True):
+        puts(colored.red('Failure load external file') + str("FAILED to Execute Clearner.bat, Collector.bat"))
+    else:
+        puts(colored.green('Load external file') + str("Successfully and run Cleaner.bat, Collector.bat"))
+        print("\n")
+
+        MugarrMal, MugarrBign = [], []
+        print("[*] Starting Algorithm name: ", str("Algorithm.Mug"))
+        strMalDir = "../malicious"
+        MugarrMal, MugarrBign = _ScanPs1_Mug(strMalDir)
+
+        print("#", "=" * 40, "#")
+        print(">>> Complete analyse with Algorithm.Mug")
+        print("\t>>> Count of detection ps1: ", str(len(MugarrMal)))
+        print("\t>>> Count of benign ps1: ", str(len(MugarrBign)))
+        print("#", "=" * 40, "#")
+
 
 
 if __name__ == "__main__":
